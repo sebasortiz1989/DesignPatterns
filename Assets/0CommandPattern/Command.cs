2804ab60653a2d19ced5567e193a2bcf5b03b11f
Sebas Ortiz <sebasortizsanta@gmail.com>
2021-04-20 21:22:44 -0500

    Commands Part 4 Replay and undo commands

--- Assets/0CommandPattern/Command.cs
+++ Assets/0CommandPattern/Command.cs
@@ -2,36 +2,56 @@ using UnityEngine;
 
 public abstract class Command
 {
-    public abstract void Execute(Animator anim);
+    public abstract void Execute(Animator anim, bool forward);
 }
 
 public class PerformJump: Command
 {
-    public override void Execute(Animator anim)
+    public override void Execute(Animator anim, bool forward)
     {
-        anim.SetTrigger("isJumping");
+        if (forward)
+            anim.SetTrigger("isJumping");
+        else
+            anim.SetTrigger("isJumpingR");
     }
 }
 
 public class PerformPunch: Command
 {
-    public override void Execute(Animator anim)
+    public override void Execute(Animator anim, bool forward)
     {
-        anim.SetTrigger("isPunching");
+        if (forward)
+            anim.SetTrigger("isPunching");
+        else
+            anim.SetTrigger("isPunchingR");
     }
 }
 
 public class PerformKick : Command
 {
-    public override void Execute(Animator anim)
+    public override void Execute(Animator anim, bool forward)
     {
-        anim.SetTrigger("isKicking");
+        if (forward)
+            anim.SetTrigger("isKicking");
+        else
+            anim.SetTrigger("isKickingR");
+    }
+}
+
+public class MoveForward : Command
+{
+    public override void Execute(Animator anim, bool forward)
+    {
+        if (forward)
+            anim.SetTrigger("isWalking");
+        else
+            anim.SetTrigger("isWalkingR");
     }
 }
 
 public class DoNothing: Command // Default
 {
-    public override void Execute(Animator anim)
+    public override void Execute(Animator anim, bool forward)
     {
         
     }
