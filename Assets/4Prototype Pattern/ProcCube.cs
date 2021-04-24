0b042d47f5a05adb9655c48ad5a8d57b78879784
Sebas Ortiz <sebasortizsanta@gmail.com>
2021-04-24 15:16:27 -0500

    Cloning a Game Object

--- Assets/4Prototype Pattern/ProcCube.cs
+++ Assets/4Prototype Pattern/ProcCube.cs
@@ -6,6 +6,30 @@ public class ProcCube: Object
 {
     public enum Cubeside { BOTTOM, TOP, LEFT, RIGHT, FRONT, BACK };
 
+    static GameObject cube;
+
+    public static GameObject Clone(Vector3 pos)
+    {
+        if (cube == null)
+        {
+            CreateCube(Vector3.zero);
+            cube.SetActive(false);
+        }
+
+        GameObject cubeClone = new GameObject();
+        cubeClone.AddComponent<MeshFilter>();
+        cubeClone.AddComponent<MeshRenderer>();
+        cubeClone.GetComponent<MeshFilter>().mesh = cube.GetComponent<MeshFilter>().mesh;
+        MeshRenderer rend = cubeClone.GetComponent<MeshRenderer>();
+        rend.material = cube.GetComponent<MeshRenderer>().material;
+        cubeClone.AddComponent<Rigidbody>();
+        cubeClone.AddComponent<BoxCollider>();
+        cubeClone.name = "Cube(Clone)";
+        cubeClone.gameObject.SetActive(true);
+        cubeClone.transform.position = pos;
+        return cubeClone;
+    }
+
     public static void CreateQuad(Cubeside side, GameObject parent)
     {
         Mesh mesh = new Mesh();
@@ -93,7 +117,7 @@ public class ProcCube: Object
 
     public static void CreateCube(Vector3 pos)
     {
-        GameObject cube = new GameObject();
+        cube = new GameObject();
         cube.AddComponent<MeshFilter>();
         cube.AddComponent<MeshRenderer>();
         CreateQuad(Cubeside.FRONT, cube);
@@ -131,7 +155,7 @@ public class ProcCube: Object
         //Add all the components of the cube just like the prefab we made before.
         cube.GetComponent<MeshFilter>().mesh = new Mesh();
         cube.GetComponent<MeshFilter>().mesh.CombineMeshes(combine);
-        cube.GetComponent<MeshFilter>().mesh.name = "Created cube";
+        cube.GetComponent<MeshFilter>().mesh.name = "Createdcube_" + Time.realtimeSinceStartup.ToString();
         MeshRenderer rend = cube.GetComponent<MeshRenderer>();
         rend.material = new Material(Shader.Find("Holistic/Plasma"));
         cube.AddComponent<Rigidbody>();
